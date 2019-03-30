98c25af3e17185191bf2676f745a95c82afa5b2d
Peter Lesslie <pclesslie@gmail.com>
2019-03-30 14:32:54 -0500

    read the gzip header

--- src/plzip.cpp
+++ src/plzip.cpp
@@ -1,6 +1,53 @@
 #include <cstdio>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+
+//  +---+---+---+---+---+---+---+---+---+---+
+//  |ID1|ID2|CM |FLG|     MTIME     |XFL|OS | (more-->)
+//  +---+---+---+---+---+---+---+---+---+---+
+struct GzipHeader
+{
+    uint8_t id1;
+    uint8_t id2;
+    uint8_t cm;
+    uint8_t flg;
+    uint32_t mtime;
+    uint8_t xfl;
+    uint8_t os;
+} __attribute__((packed));
 
 int main(int argc, char** argv)
 {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s [FILE]\n", argv[0]);
+        exit(0);
+    }
+
+    const char* filename = argv[1];
+    FILE* fp = fopen(filename, "rb");
+    if (!fp) {
+        perror("fopen");
+        exit(1);
+    }
+
+    GzipHeader hdr;
+    if (fread(&hdr, sizeof(hdr), 1, fp) != 1) {
+        fprintf(stderr, "fread: short read\n");
+        fclose(fp);
+        exit(1);
+    }
+    // TODO: handle big endian platform
+
+    printf("GzipHeader:\n");
+    printf("\tid1   = %u (0x%02x)\n", hdr.id1, hdr.id1);
+    printf("\tid2   = %u (0x%02x)\n", hdr.id2, hdr.id2);
+    printf("\tcm    = %u\n", hdr.cm);
+    printf("\tflg   = %u\n", hdr.flg);
+    printf("\tmtime = %u\n", hdr.mtime);
+    printf("\txfl   = %u\n", hdr.xfl);
+    printf("\tos    = %u\n", hdr.os);
+
+    fclose(fp);
     return 0;
 }
