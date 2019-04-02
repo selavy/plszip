1dc137be27c98aaa88fbffc193d3443621280121
Peter Lesslie <pclesslie@gmail.com>
2019-04-01 22:40:57 -0700

    Run multiple tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,9 +2,15 @@
 
 PLZIP=../build/src/plzip
 
-ORIG=test1.txt
-COMPRESSED=test1.txt.gz
-OUTPUT=${ORIG}.output
-echo "Test #1" && gzip -c $ORIG > $COMPRESSED && $PLZIP $COMPRESSED $OUTPUT > /dev/null && diff $ORIG $OUTPUT && echo "Passed." || (echo "Failed" && exit 1)
-rm -f $COMPRESSED
-rm -f $OUTPUT
+TESTS=(test1 test2)
+
+# TEST=test1
+for TEST in ${TESTS[@]};
+do
+    ORIG=${TEST}.txt
+    COMPRESSED=${TEST}.txt.gz
+    OUTPUT=${TEST}.output
+    echo -n "$TEST... " && gzip -c $ORIG > $COMPRESSED && $PLZIP $COMPRESSED $OUTPUT > /dev/null && diff $ORIG $OUTPUT && echo "Passed." || (echo "Failed" && exit 1)
+    rm -f $COMPRESSED
+    rm -f $OUTPUT
+done
