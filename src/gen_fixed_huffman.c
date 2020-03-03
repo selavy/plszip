d8a94f6765c34e32fb910f2958f79e84b072a49f
Peter Lesslie <pclesslie@gmail.com>
2020-03-03 14:54:18 -0600

    Update fixed_huffman_trees.h for new code translation

--- src/gen_fixed_huffman.c
+++ src/gen_fixed_huffman.c
@@ -1,9 +1,9 @@
+#include <assert.h>
+#include <limits.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
-#include <limits.h>
-#include <assert.h>
 
 #define MaxCodes 512
 #define MaxBitLength 16
@@ -26,9 +26,11 @@
     } while (0)
 
 #define ARRSIZE(x) (sizeof(x) / sizeof(x[0]))
+#define MAX(a, b) (a) > (b) ? (a) : (b)
 
 struct vec {
     size_t len;
+    size_t maxlen;
     uint16_t *d;
 };
 typedef struct vec vec;
@@ -36,157 +38,116 @@ FILE *out;
 
 #define WRITE(fmt, ...) fprintf(out, fmt "\n", ##__VA_ARGS__)
 
-int init_huffman_tree(vec *tree, const uint16_t *code_lengths,
-                      size_t n) {
+// clang-format off
+static const unsigned char BitReverseTable256[256] =
+{
+#   define R2(n)     n,     n + 2*64,     n + 1*64,     n + 3*64
+#   define R4(n) R2(n), R2(n + 2*16), R2(n + 1*16), R2(n + 3*16)
+#   define R6(n) R4(n), R4(n + 2*4 ), R4(n + 1*4 ), R4(n + 3*4 )
+    R6(0), R6(2), R6(1), R6(3)
+};
+// clang-format off
+
+uint16_t flip_u16(uint16_t v)
+{
+    // clang-format off
+    return
+        (BitReverseTable256[(v >> 0) & 0xff] << 8) |
+        (BitReverseTable256[(v >> 8) & 0xff] << 0) ;
+    // clang-format on
+}
+
+uint16_t flip_code(uint16_t code, size_t codelen) {
+    assert(0 < codelen && codelen <= 16);
+    code = flip_u16(code);
+    code >>= (16 - codelen);
+    return code;
+}
+
+void init_huffman_tree(vec *tree, const uint16_t *code_lengths, size_t n) {
     static size_t bl_count[MaxBitLength];
     static uint16_t next_code[MaxBitLength];
     static uint16_t codes[MaxCodes];
-
-    if (!(n < MaxCodes)) {
-        xassert(n < MaxCodes, "code lengths too long");
-        return 1;  // TODO: improve error codes
-    }
-
-    // 1) Count the number of codes for each code length. Let bl_count[N] be the
-    // number of codes of length N, N >= 1.
-    memset(&bl_count[0], 0, sizeof(bl_count));
     size_t max_bit_length = 0;
-    for (size_t i = 0; i < n; ++i) {
-        xassert(code_lengths[i] <= MaxBitLength, "Unsupported bit length");
-        ++bl_count[code_lengths[i]];
-        max_bit_length =
-            code_lengths[i] > max_bit_length ? code_lengths[i] : max_bit_length;
-    }
-    bl_count[0] = 0;
 
-    // 2) Find the numerical value of the smallest code for each code length:
-    memset(&next_code[0], 0, sizeof(next_code));
-    uint32_t code = 0;
-    for (size_t bits = 1; bits <= max_bit_length; ++bits) {
-        code = (code + bl_count[bits - 1]) << 1;
-        next_code[bits] = code;
+    xassert(n < MaxCodes, "code lengths too long");
+
+    {
+        // 1) Count the number of codes for each code length. Let bl_count[N] be the
+        // number of codes of length N, N >= 1.
+        memset(&bl_count[0], 0, sizeof(bl_count));
+        for (size_t i = 0; i < n; ++i) {
+            xassert(code_lengths[i] <= MaxBitLength, "invalid bit length: %u for value %zu", code_lengths[i], i);
+            ++bl_count[code_lengths[i]];
+            max_bit_length = MAX(code_lengths[i], max_bit_length);
+        }
+        bl_count[0] = 0;
     }
 
-    // 3) Assign numerical values to all codes, using consecutive values for all
-    // codes of the same length with the base values determined at step 2. Codes
-    // that are never used (which have a bit length of zero) must not be
-    // assigned a value.
-    memset(&codes[0], 0, sizeof(codes));
-    for (size_t i = 0; i < n; ++i) {
-        if (code_lengths[i] != 0) {
-            codes[i] = next_code[code_lengths[i]]++;
-            xassert((16 - __builtin_clz(codes[i])) <= code_lengths[i],
-                    "overflowed code length");
+    {
+        // 2) Find the numerical value of the smallest code for each code length:
+        memset(&next_code[0], 0, sizeof(next_code));
+        uint32_t code = 0;
+        for (size_t bits = 1; bits <= max_bit_length; ++bits) {
+            code = (code + bl_count[bits - 1]) << 1;
+            next_code[bits] = code;
         }
     }
 
-    // Table size is 2**(max_bit_length + 1)
-    size_t table_size = 1u << (max_bit_length + 1);
-    // tree.assign(table_size, EmptySentinel);
-    tree->d = calloc(table_size, sizeof(uint16_t));
-    tree->len = table_size;
-    for (int j = 0; j < table_size; ++j) {
-        tree->d[j] = EmptySentinel;
-    }
-    for (size_t value = 0; value < n; ++value) {
-        size_t len = code_lengths[value];
-        if (len == 0) {
-            continue;
+    {
+        // 3) Assign numerical values to all codes, using consecutive values for all
+        // codes of the same length with the base values determined at step 2. Codes
+        // that are never used (which have a bit length of zero) must not be
+        // assigned a value.
+        memset(&codes[0], 0, sizeof(codes));
+        for (size_t i = 0; i < n; ++i) {
+            if (code_lengths[i] != 0) {
+                codes[i] = next_code[code_lengths[i]]++;
+                xassert((16 - __builtin_clz(codes[i])) <= code_lengths[i], "overflowed code length");
+            }
         }
-        uint16_t code = codes[value];
-        size_t index = 1;
-        for (int i = len - 1; i >= 0; --i) {
-            size_t isset = ((code & (1u << i)) != 0) ? 1 : 0;
-            index = 2 * index + isset;
-        }
-        xassert(tree->d[index] == EmptySentinel,
-                "Assigned multiple values to same index");
-        tree->d[index] = value;
     }
 
-    return 0;
+    {
+        // 4) Generate dense table. This means that can read `max_bit_length` bits at a
+        // time, and do a lookup immediately; should then use `code_lengths` to
+        // determine how many of the peek'd bits should be removed.
+        size_t tablesz = 1u << max_bit_length;
+        tree->d = calloc(tablesz, sizeof(uint16_t));
+        tree->len = tablesz;
+        for (int j = 0; j < tablesz; ++j) {
+            tree->d[j] = EmptySentinel;
+        }
+        for (size_t i = 0; i < n; ++i) {
+            if (code_lengths[i] == 0) continue;
+            uint16_t code = codes[i];
+            uint16_t codelen = code_lengths[i];
+            uint16_t value = (uint16_t)i;
+            size_t empty_bits = max_bit_length - codelen;
+            code = (uint16_t)(code << empty_bits);
+            uint16_t lowbits = (uint16_t)((1u << empty_bits) - 1);
+            uint16_t maxcode = code | lowbits;
+            while (code <= maxcode) {
+                uint16_t flipped = flip_code(code, max_bit_length);
+                xassert(tree->d[flipped] == EmptySentinel, "reused index: %u", flipped);
+                tree->d[flipped] = value;
+                ++code;
+            }
+        }
+        tree->maxlen = max_bit_length;
+    }
 }
 
-int init_fixed_huffman(vec *lit_tree, vec *dist_tree) {
-    static uint16_t codes[288];
-    vec cvec;
-    int rc;
-
-    /* Literal Tree */
-    struct LiteralCodeTableEntry {
-        size_t start, stop, bits;
-    } xs[] = {
-        // start, stop, bits
-        {
-            0,
-            143,
-            8,
-        },
-        {
-            144,
-            255,
-            9,
-        },
-        {
-            256,
-            279,
-            7,
-        },
-        {
-            280,
-            287,
-            8,
-        },
-    };
-    for (size_t j = 0; j < ARRSIZE(xs); ++j) {
-        for (size_t i = xs[j].start; i <= xs[j].stop; ++i) {
-            codes[i] = xs[j].bits;
-        }
-    }
-    // {
-    //     size_t i = 0;
-    //     while (i < 144) codes[i++] = 8;
-    //     while (i < 256) codes[i++] = 9;
-    //     while (i < 280) codes[i++] = 7;
-    //     while (i < 288) codes[i++] = 8;
-    // }
-    // cvec.d = &codes[0];
-    // cvec.len = 288;
-    if ((rc = init_huffman_tree(lit_tree, &codes[0], LIT_CODES)) != 0) {
-        panic("failed to initialize fixed literals huffman tree.");
-        return rc;
-    }
-
-    /* Distance Tree */
-    for (size_t i = 0; i < 32; ++i) {
-        codes[i] = 5;
-    }
-    // cvec.d = &codes[0];
-    // cvec.len = 32;
-    if ((rc = init_huffman_tree(dist_tree, &codes[0], DIST_CODES)) != 0) {
-        panic("failed to initialize fixed distance huffman tree.");
-        return rc;
-    }
-
-    return 0;
-}
-
-void print_tree(vec v) {
+void print_tree(const uint16_t *A, size_t n) {
 #define FMT "%5u"
-    size_t n = v.len;
-    uint16_t *A = v.d;
-    size_t i = 0;
-    for (; i < n; i += 8) {
-        WRITE("    " FMT ", " FMT ", " FMT ", " FMT ", " FMT ", " FMT ", " FMT ", " FMT ", // (%4zu)",
-                A[i+0],
-                A[i+1],
-                A[i+2],
-                A[i+3],
-                A[i+4],
-                A[i+5],
-                A[i+6],
-                A[i+7],
+    size_t i;
+    for (i = 0; i < n; i += 8) {
+        // clang-format off
+        WRITE("    " FMT ", " FMT ", " FMT ", " FMT ", " FMT ", " FMT ", " FMT ", " FMT ",    // (%4zu)",
+                A[i + 0], A[i + 1], A[i + 2], A[i + 3],
+                A[i + 4], A[i + 5], A[i + 6], A[i + 7],
                 i);
+        // clang-format on
     }
     fprintf(out, "    ");
     for (; i < n; ++i) {
@@ -195,8 +156,11 @@ void print_tree(vec v) {
 }
 
 int main(int argc, char **argv) {
-    vec lit_tree;
-    vec dist_tree;
+    static uint16_t litlens[288];
+    static uint16_t dstlens[32];
+    vec lits;
+    vec dsts;
+    size_t i;
 
     if (argc > 1) {
         if (!(out = fopen(argv[1], "w"))) {
@@ -207,22 +171,44 @@ int main(int argc, char **argv) {
         out = stdout;
     }
 
-    if (init_fixed_huffman(&lit_tree, &dist_tree) != 0) {
-        fprintf(stderr, "ERR: failed to generate fixed huffman codes!\n");
-        exit(0);
+    {
+        i = 0;
+        while (i < 144) litlens[i++] = 8;
+        while (i < 256) litlens[i++] = 9;
+        while (i < 280) litlens[i++] = 7;
+        while (i < 288) litlens[i++] = 8;
+        init_huffman_tree(&lits, litlens, ARRSIZE(litlens));
+    }
+
+    {
+        for (i = 0; i < 32; ++i)
+            dstlens[i++] = 5;
+        init_huffman_tree(&dsts, dstlens, ARRSIZE(dstlens));
     }
 
     WRITE("#ifndef FIXED_HUFFMAN_TREES__H_");
     WRITE("#define FIXED_HUFFMAN_TREES__H_");
     WRITE("");
-    WRITE("#include <stdint.h>");
+    WRITE("#include <cstdint>");
     WRITE("");
-    WRITE("static const uint16_t fixed_huffman_literals_tree[%zu] = {", lit_tree.len);
-    print_tree(lit_tree);
+    WRITE("static constexpr uint16_t fixed_huffman_literals_maxlen = 9;");
+    WRITE("");
+    WRITE("static constexpr uint16_t fixed_huffman_literals_codelens[%zu] = {", ARRSIZE(litlens));
+    print_tree(litlens, ARRSIZE(litlens));
     WRITE("};");
     WRITE("");
-    WRITE("static const uint16_t fixed_huffman_distance_tree[%zu] = {", dist_tree.len);
-    print_tree(dist_tree);
+    WRITE("static constexpr uint16_t fixed_huffman_literals_codes[%zu] = {", lits.len);
+    print_tree(lits.d, lits.len);
+    WRITE("};");
+    WRITE("");
+    WRITE("static constexpr uint16_t fixed_huffman_distance_maxlen = 5;");
+    WRITE("");
+    WRITE("static constexpr uint16_t fixed_huffman_distance_codelens[%zu] = {", ARRSIZE(dstlens));
+    print_tree(litlens, ARRSIZE(dstlens));
+    WRITE("};");
+    WRITE("");
+    WRITE("static constexpr uint16_t fixed_huffman_distance_tree[%zu] = {", dsts.len);
+    print_tree(dsts.d, dsts.len);
     WRITE("};");
     WRITE("");
     WRITE("#endif // FIXED_HUFFMAN_TREES__H_");
