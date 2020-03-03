b8672f049938efc64a4029a899254c9d46940e64
Peter Lesslie <pclesslie@gmail.com>
2020-03-03 09:56:31 -0600

    codes verified correct for fixed table

--- src/display_huffman_codes.cpp
+++ src/display_huffman_codes.cpp
@@ -1,9 +1,10 @@
-#include <assert.h>
-#include <limits.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cassert>
+#include <climits>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
 
 #define MaxCodes 512
 #define MaxBitLength 16
@@ -27,21 +28,14 @@
 
 #define ARRSIZE(x) (sizeof(x) / sizeof(x[0]))
 
-struct vec {
-    size_t len;
-    uint16_t *d;
-};
-typedef struct vec vec;
+using Vec = std::vector<uint16_t>;
 
-int init_huffman_tree(vec *tree, const uint16_t *code_lengths, size_t n) {
+void init_huffman_tree(Vec& tree, const uint16_t *code_lengths, size_t n) {
     static size_t bl_count[MaxBitLength];
     static uint16_t next_code[MaxBitLength];
     static uint16_t codes[MaxCodes];
 
-    if (!(n < MaxCodes)) {
-        xassert(n < MaxCodes, "code lengths too long: %zu", n);
-        return 1;  // TODO: improve error codes
-    }
+    xassert(n < MaxCodes, "code lengths too long: %zu", n);
 
     // 1) Count the number of codes for each code length. Let bl_count[N] be the
     // number of codes of length N, N >= 1.
@@ -76,12 +70,8 @@ int init_huffman_tree(vec *tree, const uint16_t *code_lengths, size_t n) {
 
     // Table size is 2**(max_bit_length + 1)
     size_t table_size = 1u << (max_bit_length + 1);
-    // tree.assign(table_size, EmptySentinel);
-    tree->d = reinterpret_cast<uint16_t*>(calloc(table_size, sizeof(uint16_t)));
-    tree->len = table_size;
-    for (size_t j = 0; j < table_size; ++j) {
-        tree->d[j] = EmptySentinel;
-    }
+    tree.assign(table_size, EmptySentinel);
+    assert(tree.size() == table_size);
     for (size_t value = 0; value < n; ++value) {
         int len = static_cast<int>(code_lengths[value]);
         if (len == 0) {
@@ -93,68 +83,42 @@ int init_huffman_tree(vec *tree, const uint16_t *code_lengths, size_t n) {
             size_t isset = ((code & (1u << i)) != 0) ? 1 : 0;
             index = 2 * index + isset;
         }
-        xassert(tree->d[index] == EmptySentinel, "Assigned multiple values to same index: %zu", index);
-        tree->d[index] = static_cast<uint16_t>(value);
+        xassert(tree[index] == EmptySentinel, "Assigned multiple values to same index: %zu", index);
+        tree[index] = static_cast<uint16_t>(value);
     }
-
-    return 0;
 }
 
-int init_fixed_huffman(vec *lit_tree, vec *dist_tree) {
+void init_fixed_huffman(Vec& lit_tree, Vec& dist_tree) {
     static uint16_t codes[288];
-    int rc;
-    struct LiteralCodeTableEntry {
-        size_t start, stop, bits;
-    } xs[] = {
-        {
-            0,
-            143,
-            8,
-        },
-        {
-            144,
-            255,
-            9,
-        },
-        {
-            256,
-            279,
-            7,
-        },
-        {
-            280,
-            287,
-            8,
-        },
-    };
-    for (size_t j = 0; j < ARRSIZE(xs); ++j) {
-        for (size_t i = xs[j].start; i <= xs[j].stop; ++i) {
-            codes[i] = static_cast<uint16_t>(xs[j].bits);
-        }
+    {
+        size_t i = 0;
+        while (i <= 143)
+            codes[i++] = 8;
+        while (i <= 255)
+            codes[i++] = 9;
+        while (i <= 279)
+            codes[i++] = 7;
+        while (i <= 287)
+            codes[i++] = 8;
     }
-    if ((rc = init_huffman_tree(lit_tree, &codes[0], LIT_CODES)) != 0) {
-        panic0("failed to initialize fixed literals huffman tree.");
-        return rc;
-    }
+    init_huffman_tree(lit_tree, &codes[0], LIT_CODES);
+
     for (size_t i = 0; i < 32; ++i) {
         codes[i] = 5;
     }
-    if ((rc = init_huffman_tree(dist_tree, &codes[0], DIST_CODES)) != 0) {
-        panic0("failed to initialize fixed distance huffman tree.");
-        return rc;
-    }
-
-    return 0;
+    init_huffman_tree(dist_tree, &codes[0], DIST_CODES);
 }
 
 int main(int argc, char **argv) {
     (void)argc;
     (void)argv;
-    vec lit_tree;
-    vec dist_tree;
-    if (init_fixed_huffman(&lit_tree, &dist_tree) != 0) {
-        fprintf(stderr, "ERR: failed to generate fixed huffman codes!\n");
-        exit(0);
+    Vec lits, dsts;
+    init_fixed_huffman(lits, dsts);
+    for (size_t i = 0; i < lits.size(); ++i) {
+        printf("lits[%zu] = %u\n", i, lits[i]);
+    }
+    for (size_t i = 0; i < dsts.size(); ++i) {
+        printf("dsts[%zu] = %u\n", i, dsts[i]);
     }
     return 0;
 }
